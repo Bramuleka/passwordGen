ffeb9ab65c5c72afc85d003c09158818ed1e4b7d
bramuleka <bramuleka@gmail.com>
2022-06-10 23:59:13 +0300

    Updated files  Please enter the commit messag for your changes. Lines starting

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 function copyToClipboard(){
-    var copyText = document.getElementById("output");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-    var x = document.getElementById("snackbar");
-  x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  var copyText = document.getElementById("output");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  document.getElementById('icon').src="img/lockanimated.gif"
+  document.getElementById('copied').src="img/Asset 2.png"
+  var x = document.getElementById("snackbar");
+x.className = "show";
+setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
 
+      var slider = document.getElementById("myRange");
+      var output = document.getElementById("demo");
+      output.innerHTML = slider.value;
+      slider.oninput = function() {
+              output.innerHTML = this.value;
+          }
 
-var password=document.getElementById("output");
+  var password=document.getElementById("output");
+  function genPass() {
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+[]{};:'~|\/<>,.?";
+  
+  //ÇüéâäàåçêëèïîìæÆôöòûùÿ¢£¥PƒáíóúñÑ¿¬½¼¡«»¦ßµ±°•·²€„…†‡ˆ‰Š‹Œ‘’“”–—˜™š›œŸ¨©®¯³´¸¹¾ÀÁÂÃÄÅÈÉÊËÌÍÎÏÐÒÓÔÕÖ×ØÙÚÛÜÝÞãðõ÷øüýþ
 
- function genPass() {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+[]{};:'~|\/<>,.?";
-    var passwordLength = 20;
-    var password = "";
- for (var i = 0; i <= passwordLength; i++) {
-   var randomNumber = Math.floor(Math.random() * chars.length);
-   password += chars.substring(randomNumber, randomNumber +1);
-  }
-        document.getElementById("output").value = password;
- }
+  var passwordLength = document.getElementById("myRange").value;
+  var password = "";
+  for (var i = 0; i <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
+      }
+      document.getElementById("output").value = password;
+      document.getElementById('icon').src="img/Lock.png"
+      document.getElementById('copied').src="img/Asset 1.png"
+}
